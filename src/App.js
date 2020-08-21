3e818c0106ad87498527c7c97a06fe1081da7a98
teddyhn <tngo97@gmail.com>
2020-08-21 16:16:55 -0700

    Set up project with game board

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+// R3F
+import { Canvas } from "react-three-fiber";
+// Drei - R3F
+import { softShadows, OrbitControls, Box } from "drei";
+// Components
+import Header from "./components/Header.js";
+// Styles
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// soft Shadows
+softShadows();
+
+const createGrid = (gridSize) => {
+  // Create one dimensional array 
+  let grid = new Array(gridSize); 
+    
+  // Loop to create 2D array using 1D array 
+  for (let i = 0; i < grid.length; i++) { 
+    grid[i] = new Array(gridSize); 
+  }
+    
+  // Loop to initialize 2D array elements. 
+  for (let i = 0; i < gridSize; i++) { 
+    for (let j = 0; j < gridSize; j++) { 
+      grid[i][j] = `${i},${j}`; 
+    } 
+  }
+
+  return grid
 }
+
+const App = () => {
+  const [grid, setGrid] = useState([])
+
+  useEffect(() => {
+    setGrid(createGrid(7))
+  }, [])
+
+  return (
+    <>
+      <Header />
+      <Canvas
+        camera={{ position: [0, 4, 10], fov: 60 }}>
+        <group>
+          { grid.map((row, i) => row.map((tile, j) => 
+            <Box key={tile} args={[1, 0.25, 1]} position={[j - 3, 0, i - 3]}>
+              { (j % 2) === (i % 2) ? <meshBasicMaterial onClick={console.log('hi')} attach="material" color="hotpink" /> : <meshBasicMaterial attach="material" color="lightgreen" /> }
+            </Box>
+          ))}
+        </group>
+        <OrbitControls />
+      </Canvas>
+    </>
+  );
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className='logo'>
+        <span>3D CONNECT FOUR</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
