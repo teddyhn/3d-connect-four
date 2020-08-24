93e2aeb3f1221ae9903d8363e5aa17bf7c9a53d6
teddyhn <tngo97@gmail.com>
2020-08-24 11:01:53 -0700

    Grid rendering more efficient

--- src/App.js
+++ src/App.js
@@ -1,53 +1,98 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 // R3F
-import { Canvas } from "react-three-fiber";
+import { Canvas } from "react-three-fiber"
 // Drei - R3F
-import { softShadows, OrbitControls, Box } from "drei";
+import { softShadows, OrbitControls, Box } from "drei"
 // Components
-import Header from "./components/Header.js";
+import Header from "./components/Header.js"
+import Balls from "./components/Balls.js"
 // Styles
-import "./App.scss";
+import "./App.scss"
 
 // soft Shadows
-softShadows();
+softShadows()
 
 const createGrid = (gridSize) => {
   // Create one dimensional array 
-  let grid = new Array(gridSize); 
+  const grid = new Array(gridSize)
     
   // Loop to create 2D array using 1D array 
   for (let i = 0; i < grid.length; i++) { 
-    grid[i] = new Array(gridSize); 
+    grid[i] = new Array(gridSize);
   }
     
   // Loop to initialize 2D array elements. 
   for (let i = 0; i < gridSize; i++) { 
     for (let j = 0; j < gridSize; j++) { 
-      grid[i][j] = `${i},${j}`; 
+      grid[i][j] = [] 
     } 
   }
 
   return grid
 }
 
+const updateGrid = (grid = [], ball = {}) => {
+  if (!grid.length) grid = createGrid(7)
+
+  if (Object.keys(ball).length) grid[ball.row][ball.column].push(ball.color)
+
+  return grid
+}
+
 const App = () => {
   const [grid, setGrid] = useState([])
+  const [color, setColor] = useState("blue")
 
+  // Initialize grid on render
   useEffect(() => {
-    setGrid(createGrid(7))
+    setGrid(updateGrid())
   }, [])
+
+  const handleClick = (e, i, j) => {
+    e.stopPropagation()
+
+    color === "blue" ? setColor("red") : setColor("blue")
+
+    setGrid(updateGrid(grid, { row: j, column: i, color: color }))
+  }
 
   return (
     <>
       <Header />
       <Canvas
+        shadowMap
         camera={{ position: [0, 4, 10], fov: 60 }}>
+        <ambientLight intensity={1.5} />
+        <directionalLight
+          castShadow
+          position={[-7, 10, 10]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
         <group>
-          { grid.map((row, i) => row.map((tile, j) => 
-            <Box key={tile} args={[1, 0.25, 1]} position={[j - 3, 0, i - 3]}>
-              { (j % 2) === (i % 2) ? <meshBasicMaterial onClick={console.log('hi')} attach="material" color="hotpink" /> : <meshBasicMaterial attach="material" color="lightgreen" /> }
+          {/* 2D plane that receives shadows, sits on top of board */}
+          <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, 0.125 + 0.001, 0]}
+            receiveShadow>
+            <planeBufferGeometry attach="geometry" args={[7, 7]} />
+            <shadowMaterial attach="material" opacity={0.3} />
+          </mesh>
+
+          {grid.map((row, i) => row.map((tile, j) => 
+            <Box onClick={e => handleClick(e, i, j)} key={`${i}, ${j}`} args={[1, 0.25, 1]} position={[j - 3, 0, i - 3]} receiveShadow>
+              { (j % 2) === (i % 2) ? <meshBasicMaterial attach="material" color="gray" /> : <meshBasicMaterial attach="material" color="white" /> }
             </Box>
           ))}
+
+          <Balls grid={grid} />
+          
         </group>
         <OrbitControls />
       </Canvas>
---
+++ src/components/Balls.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Sphere } from "drei"
+
+const Balls = ({ grid }) => {
+
+    return (
+        <>
+            {grid.map((row, i) => 
+                row.map((tile, j) => 
+                    tile.map((item, k) => 
+                        <Sphere key={`${i},${j},${k}`} castShadow args={[0.4, 64, 64]} position={[i - 3, k + 0.6125, j - 3]}>
+                            <meshPhongMaterial attach="material" color={item} emissive={0x444444} />
+                        </Sphere>
+                    )
+                )
+            )}
+        </>
+    );
+};
+
+export default Balls;
